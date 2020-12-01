242584a46df2c8c23f5d2fa311dfaed66ec4011a
adelbertclerge <adelbertclerge@gmail.com>
2020-12-01 16:45:06 -0500

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,18 @@
+class Dog
+    def initilize(name, breed)
+        @name = Fluff
+        @breed = Corgi
+    end
+    def name=(name)
+        @name = name
+    end
+    def breed=(breed)
+        @breed = breed 
+    end 
+    def name
+        @name
+    end
+    def breed
+        @breed
+    end
+end
---
+++ lib/person.rb
@@ -0,0 +1,18 @@
+class Person
+    def initilize(name, job)
+        @name = Adelbert
+        @job = Software Engineer 
+    end
+    def name=(name)
+        @name = name
+    end
+    def job=(job)
+        @job = job 
+    end 
+    def name
+        @name
+    end
+    def job
+        @job
+    end
+end
